2cf7b1eb72c36e659bbea91f84d89d41e1d41ea2
Krzysztof Zyczenko <krzysztof.t.zyczenko@aib.ie>
2018-11-08 15:15:54 +0000

    WIP

--- attributes/configure.rb
+++ attributes/configure.rb
@@ -1,2 +1,4 @@
 default['blue_ocean']['jenkins_username'] = 'admin'
 default['blue_ocean']['jenkins_password'] = 'admin'
+default['blue_ocean']['jenkins_url'] = 'http://localhost:8080/'
+default['blue_ocean']['admin_address'] = 'admin@foo.com'
